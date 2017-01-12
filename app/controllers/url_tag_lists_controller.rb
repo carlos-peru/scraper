f84898f9f626f8bd6612645a427bfb22defcfe0e
Carlos Castro <developer@CarlosCastro.me>
2017-01-12 00:48:35 -0800

    Controller defaults protocol to http when missing. Controller validates url and handles error connections. View displays feedback when results are empty.

--- app/controllers/url_tag_lists_controller.rb
+++ app/controllers/url_tag_lists_controller.rb
@@ -2,39 +2,49 @@ class UrlTagListsController < ApplicationController
 
   # GET /
   def search
-    if params[:url] && !params[:url].empty?
+    if !params[:url].blank?
       # Remove leading and trailing white spaces
-      url = params[:url].strip
+      @url = params[:url].strip
       # Append http protocol if needed
-      url = "http://#{url}" if !url.include? "://"
+      @url = "http://#{@url}" if !@url.include? "://"
 
-      # Check if url has been processed already
-      @url_tag_list = UrlTagList.where(url: url).take
-      # If result is older than a week, then we do a new search
-      # This is arbitrary, so 1.minute.ago or 10.seconds.ago could have been used
-      cache_timeout = 10.seconds.ago
-      # NOTE: Remember that the smaller the value of the time, the older it is.
-      if @url_tag_list.nil? || @url_tag_list.updated_at < cache_timeout
-        uri = URI(url)
-        response = Net::HTTP.get(uri)
-        html = Nokogiri::HTML(response)
-        # Create empty tag list string
-        tag_list = ""
-        # This is equivalent to traversing the nodes recursively
-        # For more information: http://www.rubydoc.info/github/sparklemotion/nokogiri/Nokogiri/XML/Node#traverse-instance_method
-        html.traverse do |node|
-          if Nokogiri::XML::Element == node.class
-            # Append tag
-            tag_list << ", " << node.name
+      if is_url_valid(@url)
+
+        # Check if url has been processed already
+        @url_tag_list = UrlTagList.where(url: @url).take
+        # If result is older than a week, then we do a new search
+        # This is arbitrary, so 1.minute.ago, 10.seconds.ago or any other timeframe could have been used
+        cache_timeout = 1.week.ago
+        # NOTE: Remember that the smaller the value of the time, the older it is.
+        if @url_tag_list.nil? || @url_tag_list.updated_at < cache_timeout
+          uri = URI(@url)
+          begin
+            response = Net::HTTP.get(uri)
+            html = Nokogiri::HTML(response)
+            # Create empty tag list string
+            tag_list = ""
+            # This is equivalent to traversing the nodes recursively
+            # For more information: http://www.rubydoc.info/github/sparklemotion/nokogiri/Nokogiri/XML/Node#traverse-instance_method
+            html.traverse do |node|
+              if Nokogiri::XML::Element == node.class
+                # Append tag
+                tag_list << ", " << node.name
+              end
+            end
+            # Remove leading comma (,) from tags
+            cache_processed_url(@url, tag_list[2..-1])
+          rescue SocketError => error
+            logger.error "Failed to connect to the given url: #{error}"
           end
         end
-        # Remove leading comma (,) from tags
-        cache_processed_url(url, tag_list[2..-1])
       end
     end
   end
 
   private
+    def is_url_valid(url)
+      !(@url =~ /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix).nil?
+    end
 
     def cache_processed_url(url, tags)
       # If it exists update
